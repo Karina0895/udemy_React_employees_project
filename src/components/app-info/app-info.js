a17bd2a149f3fa7123b814849785933151870fe5
Karina0895 <karinagubskaya0895@gmail.com>
2025-09-22 12:05:06 +0300

    created project and worked with states

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Облік співробітників компанії N</h1>
+            <h2>Загальна кількість співробітників: </h2>
+            <h2>Премію отри мають: </h2>
+        </div>
+    )
+}
+
+ export default AppInfo;
---
+++ src/components/employees-list/employees-list.js
@@ -0,0 +1,19 @@
+import EmployersListItem from "../employees-list-item/employees-list-item"
+import './employees-list.css'
+
+const EmployeesList = ({data}) => {
+
+    const elements = data.map(item => {
+        return (
+           <EmployersListItem {...item}/>    
+        )
+    });
+
+    return (
+        <ul className="app-list list-group">
+            {elements}
+        </ul>
+    )
+}
+
+export default EmployeesList;
